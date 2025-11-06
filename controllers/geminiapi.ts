87e9e24a2ad8c3df5803a1116a6bf046bc218b1a
Rohit lodhi <142474520+Rohitlodhii@users.noreply.github.com>
2025-11-06 16:15:32 +0530

    Update generative model from 'gemini-1.5' to 'gemini-2.5'

--- controllers/geminiapi.ts
+++ controllers/geminiapi.ts
@@ -7,7 +7,7 @@ interface GenerateProps {
 
 export const generateContent = async ({call} : GenerateProps) => {
     const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
-    const model = genAI.getGenerativeModel({ model : "gemini-1.5-flash"});
+  const model = genAI.getGenerativeModel({ model : "gemini-2.5-flash"});
 
 
     const prompt = `input : [${call}]
